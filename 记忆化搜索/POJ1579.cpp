681bafd60d0aa532246d9c2fea21a9cb2f1ee36b
flyhero <921285003@qq.com>
2018-03-07 14:01:19 +0800

    new content "记忆化搜索“ has been added.

---
+++ 记忆化搜索/POJ1579.cpp
@@ -0,0 +1,34 @@
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<cstdio>
+using namespace std;
+typedef long long ll;
+const int maxn = 1000005;
+
+ll mem[21][21][21];
+int a, b, c;
+
+
+ll w(ll a, ll b, ll c) {
+    if(a <= 0 || b <= 0 || c <= 0) return 1;
+    if(mem[a][b][c] != 0)
+        return mem[a][b][c];
+    if(a < b && b < c)
+        return mem[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
+    else
+        return mem[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+
+}
+
+int main() {
+    while(true) {
+        cin >> a >> b >> c;
+        if(a == -1 && b == -1 && c == -1) break;
+        ll ans;
+        if(a <= 0 || b <= 0 || c <= 0) ans = 1;
+        else if(a > 20 || b > 20 || c > 20) ans = w(20, 20, 20);
+        else ans = w(a, b, c);
+        cout << "w(" << a << ", " << b << ", " << c << ") = " << ans << endl;
+    }
+    return 0;
+}
---
+++ 记忆化搜索/斐波那契（简单记忆化搜索）.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int maxn = 50001;
+
+ll mem[maxn];
+
+ll fibb(int n) {
+    if(n <= 1) return (ll) n;
+    if(mem[n] != 0) return mem[n];
+    return mem[n] = fibb(n-1) + fibb(n-2);
+}
+
+int main() {
+    int n;
+    while(cin >> n) {
+        cout << fibb(n) << endl;
+    }
+    return 0;
+}
