1af0844d3680a041865e7f7078788a5c1caa4a77
flyhero <921285003@qq.com>
2018-03-31 20:54:17 +0800

    update

--- 背包问题/多重背包/POJ1276（多重背包模板题）.cpp
+++ 背包问题/多重背包/POJ1276（多重背包模板题）.cpp
@@ -1,4 +1,10 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<algorithm>
+#include<cmath>
+#include<cstring>
 using namespace std;
 #define fi first
 #define se second
@@ -8,48 +14,49 @@ using namespace std;
 #define pii pair<int,int>
 #define mem(a,b) memset(a,b,sizeof(a))
 
-const int maxn = 100005;
-const int maxm = 10005;
+const int maxn = 10005;
+const int maxm = 100005;
 
-int cash, n;
-int dp[maxn];
-int c[maxn], w[maxn], num[maxn];
+int n, m;
+int cost[maxn];
+int value[maxn];
+int num[maxn]; // 每种物品的数量
+int dp[maxm];
 
-void ZeroOne_Pack(int cost, int weight) {
-	for(int i = cash;i >= cost;i--) dp[i] = max(dp[i], dp[i-cost]+weight);
+void ZeroOnePack(int c, int v) { // 对单个物品01背包
+    for(int i = m; i >= c; i--) dp[i] = max(dp[i], dp[i-c]+v);
 }
 
-void Complete_Pack(int cost, int weight) {
-	for(int i = cost;i <= cash;i++) dp[i] = max(dp[i], dp[i-cost]+weight);
+void CompletePack(int c, int v) { // 对单个物品完全背包
+    for(int i = c; i <= m; i++) dp[i] = max(dp[i], dp[i-c]+v);
 }
 
-int Multi_Pack() {
-	memset(dp, 0, sizeof(dp));
-	for(int i = 1;i <= n;i++) {
-		if(num[i]*c[i] > cash) { //物品全装进去超重，说明物品无限，直接套完全背包
-			Complete_Pack(c[i], w[i]);
-		}
-		else {
-			int k = 1;
-			while(k < num[i]) { //利用二进制思想，把物品分成1，2，4，8个，依次01背包，能凑出任何数值，降复杂度
-				ZeroOne_Pack(k*c[i], k*w[i]);
-				num[i] -= k;
-				k <<= 1;
-			}
-			ZeroOne_Pack(num[i]*c[i], num[i]*w[i]);
-		}
-	}
-	return dp[cash];
+int MultiPack() {
+    mem(dp, 0);
+    for(int i = 1; i <= n; i++) { // 枚举每种物品
+        if(num[i]*cost[i] > m) { // 物品全装进去超重，说明物品无限，直接套完全背包
+            CompletePack(cost[i], value[i]);
+        }
+        else {
+            int k = 1;
+            while(k < num[i]) { // 利用二进制思想，把物品分成1，2，4，8个，依次01背包，能凑出任何数值，降低复杂度
+                ZeroOnePack(k*cost[i], k*value[i]);
+                num[i] -= k;
+                k *= 2;
+            }
+            ZeroOnePack(num[i]*cost[i], num[i]*value[i]);
+        }
+    }
+    return dp[m];
 }
 
 int main() {
-	while(cin >> cash >> n) {
-		for(int i = 1;i <= n;i++) {
-			cin >> num[i] >> c[i];
-			w[i] = c[i];
-		}
-		cout << Multi_Pack() << endl;
-	}
-	
-	return 0;
+    while(cin >> m >> n) {
+        for(int i = 1;i <= n;i++) {
+            cin >> num[i] >> cost[i];
+            value[i] = cost[i];
+        }
+        cout << MultiPack() << endl;
+    }
+    return 0;
 }
