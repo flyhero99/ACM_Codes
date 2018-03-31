673010032f210830e63ef8e2b9d787e3217473cd
flyhero <921285003@qq.com>
2018-03-31 20:31:01 +0800

    update

--- 背包问题/完全背包/洛谷1616（完全背包裸题）.cpp
+++ 背包问题/完全背包/洛谷1616（完全背包裸题）.cpp
@@ -8,21 +8,27 @@ using namespace std;
 #define pii pair<int,int>
 #define mem(a,b) memset(a,b,sizeof(a))
 
-const int maxt = 100005;
-const int maxm = 10005;
+const int maxn = 10005; // 最大物品种类数
+const int maxm = 100005; // 最大背包容量
 
-int t, m;
-int c[maxm];
-int v[maxm];
-int dp[maxt];
+int n, m; // n-物品种类数量，m-背包最大容量
+int cost[maxn];
+int value[maxn];
+int dp[maxm];
+
+void CompletePack() {
+    for(int i = 0; i < n; i++) { // 
+        for(int j = cost[i]; j <= m; j++) { // 把01背包的过程反过来就是完全背包，因为同一物品可以选多次
+            dp[j] = max(dp[j], dp[j-cost[i]] + value[i]);
+        }
+    }
+}
 
 int main() {
-	while(cin >> t >> m) {
-		for(int i = 0;i < m;i++) cin >> c[i] >> v[i];
-		for(int i = 0;i < m;i++) {
-			for(int j = c[i];j <= t;j++) dp[j] = max(dp[j], dp[j-c[i]]+v[i]);
-		}
-		cout << dp[t] << endl;
-	}
-	return 0;
+    while(cin >> m >> n) {
+        for(int i = 0; i < n; i++) cin >> cost[i] >> value[i];
+        CompletePack();
+        cout << dp[m] << endl;
+    }
+    return 0;
 }
