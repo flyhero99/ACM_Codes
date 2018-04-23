53096d37d028d0e40a04210d017ed7e8f56ebfd3
flyhero <921285003@qq.com>
2018-04-23 13:13:30 +0800

    update

--- 2018ccpc暨丝绸之路B 判断质数miller_robin板子.cpp
+++ 2018ccpc暨丝绸之路B 判断质数miller_robin板子.cpp
@@ -43,98 +43,91 @@ const int S = 10;
 //your code goes here
 
 //O(n)
-const int maxn = 1e5;
+const int maxn = 1e5+5;
+
 bool vis[maxn];
-int prime[maxn / 10], tot = 0;
-void init(){
-        memset(vis,0,sizeof(vis)); tot = 0;
-        for(int i = 2; i < maxn; ++i){
-                if(!vis[i]) prime[tot++] = i;
-                for(int j = 0;j < tot; ++j){
-                        if(i * prime[j] > maxn) break;
-                        vis[i*prime[j]] = 1;
-                        if(i % prime[j] == 0) break;
-                }
+int prime[maxn/10], tot = 0;
+
+void init() {
+    memset(vis, 0, sizeof(vis));
+    tot = 0;
+    for(int i = 2; i < maxn; ++i) {
+        if(!vis[i]) prime[tot++] = i;
+        for(int j = 0;j < tot; ++j) {
+            if(i * prime[j] > maxn) break;
+            vis[i*prime[j]] = 1;
+            if(i % prime[j] == 0) break;
         }
+    }
 }
 
-LL random(LL n)
-{
-    return (LL)((double)rand()/RAND_MAX*n+0.5);
+LL random(LL n) {
+    return (LL) ((double)rand()/RAND_MAX*n+0.5);
 }
-LL multi(LL a,LL b,LL m) //计算a*b%m
-{
-    LL ret=0;
-    while(b)
-    {
-        if(b&1) ret=(ret+a)%m;
-        b>>=1;
-        a=(a<<1)%m;
+
+LL multi(LL a, LL b, LL m) {//计算a*b%m
+    LL ret = 0;
+    while(b) {
+        if(b & 1) ret = (ret+a) % m;
+        b >>= 1;
+        a = (a << 1) % m;
     }
     return ret;
 }
-LL quick_mod(LL a,LL b,LL m) //计算a^b%m
-{
-    LL ans=1;
-    while(b)
-    {
-        if(b&1)
-        {
-            ans=multi(ans,a,m);
+
+LL quick_mod(LL a, LL b, LL m) {//计算a^b%m
+    LL ans = 1;
+    while(b) {
+        if(b & 1) {
+            ans = multi(ans, a, m);
             b--;
         }
-        b>>=1;
-        a=multi(a,a,m);
+        b >>= 1;
+        a = multi(a, a, m);
     }
     return ans;
 }
-bool miller_rabbin(LL n)
-{
-    if(n==2) return true;
-    if(n<2||!(n&1)) return false;
-    LL m=n-1;
-    int k=0;
-    while((m&1)==0)
-    {
+
+bool miller_rabbin(LL n) {
+    if(n == 2) return true;
+    if(n < 2 || !(n&1)) return false;
+    LL m = n-1;
+    int k = 0;
+    while((m & 1) == 0) {
         k++;
-        m>>=1;
+        m >>= 1;
     }
-    for(int i=0; i<N; i++)
-    {
-        LL a=rand()%(n - 1)+1;
-        LL x=quick_mod(a,m,n);
-        LL y=0;
-        for(int j=0;j<k;j++)
-        {
-            y=multi(x,x,n);
-            if(y==1&&x!=1&&x!=n-1) return false;
-            x=y;
+    for(int i = 0; i < N; i++) {
+        LL a = rand()%(n - 1) + 1;
+        LL x = quick_mod(a, m, n);
+        LL y = 0;
+        for(int j = 0; j < k; j++) {
+            y = multi(x, x, n);
+            if(y == 1 && x != 1 && x != n-1) return false;
+            x = y;
         }
-        if(y!=1) return false;
+        if(y != 1) return false;
     }
     return true;
 }
+
 //O(n)
-int main()
-{
-//        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-//        cout << miller_rabbin(9223372036854775783ll) << endl;
-        init();
-        int T; scanf("%d",&T);
-        prime[0] = 2;
-        while(T--)
-        {
-                ll n; scanf("%llu",&n);
-                for(int i = 0; i < tot; ++i)
-                {
-                        ll qwq = n-prime[i];
-//                        cout << qwq << ' ' << prime[i] << endl;
-                        if(miller_rabbin(qwq))
-                        {
-                                printf("%d %llu\n",prime[i],qwq);
-                                break;
-                        }
-                }
+int main() {
+    // ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    // cout << miller_rabbin(9223372036854775783ll) << endl;
+    init();
+    int T; scanf("%d", &T);
+    prime[0] = 2;
+    while(T--) {
+        ll n; scanf("%llu", &n);
+        for(int i = 0; i < tot; ++i) {
+            ll qwq = n - prime[i];
+            // cout << qwq << ' ' << prime[i] << endl;
+            if(miller_rabbin(qwq)) {
+                printf("%d %llu\n", prime[i], qwq);
+                break;
+            }
         }
-        return 0;
+    }
+    return 0;
 }
